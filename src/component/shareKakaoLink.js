828878d8d10cda65c10487e39260172be425e38e
wonsunyoung <wonsunyoung0703@gmail.com>
2023-10-17 17:12:51 +0900

    Update shareKakaoLink.js

--- src/component/shareKakaoLink.js
+++ src/component/shareKakaoLink.js
@@ -1,30 +1,88 @@
-export const shareKakao = (route, title) => { // url이 id값에 따라 변경되기 때문에 route를 인자값으로 받아줌
+import { useEffect } from 'react'
+
+export default function KakaoShare() {
+	useEffect(() => {
+    kakaoButton()
+  }, [])
+
+  const kakaoButton = () => {
     if (window.Kakao) {
-      const kakao = window.Kakao;
+      const kakao = window.Kakao
+
       if (!kakao.isInitialized()) {
-        kakao.init("206dac13674d5431132f3d0db85f378f"); // 카카오에서 제공받은 javascript key를 넣어줌 -> .env파일에서 호출시킴
+        kakao.init('206dac13674d5431132f3d0db85f378f')
       }
-  
-      kakao.Link.sendDefault({
-        objectType: "feed", // 카카오 링크 공유 여러 type들 중 feed라는 타입 -> 자세한 건 카카오에서 확인
+
+      kakao.Share.createDefaultButton({
+        container: '#kakaotalk-sharing-btn',
+        objectType: 'feed',
         content: {
-          title: title, // 인자값으로 받은 title
-          description: "설명", // 인자값으로 받은 title
-          imageUrl: "이미지 url",
+          title: '오늘의 디저트',
+          description: '아메리카노, 빵, 케익',
+          imageUrl:
+            'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
           link: {
-            mobileWebUrl: route, // 인자값으로 받은 route(uri 형태)
-            webUrl: route
-          }
+            mobileWebUrl: 'https://developers.kakao.com',
+            webUrl: 'https://developers.kakao.com',
+          },
+        },
+        itemContent: {
+          profileText: 'Kakao',
+          profileImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+          titleImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+          titleImageText: 'Cheese cake',
+          titleImageCategory: 'Cake',
+          items: [
+            {
+              item: 'Cake1',
+              itemOp: '1000원',
+            },
+            {
+              item: 'Cake2',
+              itemOp: '2000원',
+            },
+            {
+              item: 'Cake3',
+              itemOp: '3000원',
+            },
+            {
+              item: 'Cake4',
+              itemOp: '4000원',
+            },
+            {
+              item: 'Cake5',
+              itemOp: '5000원',
+            },
+          ],
+          sum: 'Total',
+          sumOp: '15000원',
+        },
+        social: {
+          likeCount: 10,
+          commentCount: 20,
+          sharedCount: 30,
         },
         buttons: [
           {
-            title: "title",
+            title: '웹으로 이동',
             link: {
-              mobileWebUrl: route,
-              webUrl: route
-            }
-          }
-        ]
+              mobileWebUrl: 'https://developers.kakao.com',
+              webUrl: 'https://developers.kakao.com',
+            },
+          },
+          {
+            title: '앱으로 이동',
+            link: {
+              mobileWebUrl: 'https://developers.kakao.com',
+              webUrl: 'https://developers.kakao.com',
+            },
+          },
+        ],
       });
     }
-  };
+  }
+	
+	return (
+		<button id='kakaotalk-sharing-btn'>KakaoShare</button>
+	)
+} 
