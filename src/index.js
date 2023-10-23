90285244b5f7c1f924dee170e02a66018df51248
wonsunyoung <wonsunyoung0703@gmail.com>
2023-10-24 01:16:47 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import logo from '../src/asset/image/logo.png'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div style={{width : '90px', margin:'0 auto',marginBottom:'24px'}}><img src={logo} style={{borderRadius:'0px'}}></img></div>
     <App />
   </React.StrictMode>
 );
