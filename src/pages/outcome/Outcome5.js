3f04d46b5e8094bcbb33d42583c01926efca163b
wonsunyoung <wonsunyoung0703@gmail.com>
2023-10-24 01:17:03 +0900

    Create Outcome5.js

---
+++ src/pages/outcome/Outcome5.js
@@ -0,0 +1,90 @@
+import { useNavigate, useLocation, Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import fourth from '../../asset/image/fourth.jpeg'
+import virtupia from '../../asset/image/virtupia.png'
+import appstore from '../../asset/image/appstore.png'
+import googlestore from '../../asset/image/googleplay.png'
+import {FacebookShareButton, FacebookIcon, FacebookMessengerShareButton, FacebookMessengerIcon, TwitterShareButton, TwitterIcon, LineShareButton, LineIcon,} from "react-share";
+
+export default function Outcome5(){
+const appStoreUrl = "https://apps.apple.com/kr/app/%EB%B2%84%ED%8A%9C%ED%94%BC%EC%95%84/id6451460797"
+const googleStroeeUrl = "https://play.google.com/store/apps/details?id=com.virtuverseapp&pcampaignid=web_share"
+const navigate = useNavigate();
+const restart = () => {navigate('/')}
+const location = useLocation();
+const kakaoButton = () => {
+    if (window.Kakao) {
+      const kakao = window.Kakao
+      if (!kakao.isInitialized()) {
+        kakao.init('206dac13674d5431132f3d0db85f378f')
+      }
+
+      kakao.Share.sendDefault({
+        objectType: 'feed',
+        content: {
+          title: '덕후 판독기',
+          description: '나는 덕질에 얼마나 진심일까?',
+          imageUrl:
+            'https://ifh.cc/g/rR3gVj.jpg',
+          link: {
+            webUrl: 'https://virtupiaotaku.netlify.app/',
+          },
+        },
+        social: {
+          likeCount: 286,
+          commentCount: 45,
+          sharedCount: 845,
+        },
+        buttons: [
+          {
+            title: '웹으로 보기',
+            link: {
+              webUrl: 'https://virtupiaotaku.netlify.app/',
+            },
+          },
+        ],
+      });
+    }
+  }
+
+
+    return(
+    <div className='result'>
+        <div className='up'>
+            <p className='ques'>당신의 덕력은..?</p>
+            <div className='content'>
+                <b>궁극의 십덕</b>
+                <img src={fourth}></img>
+                <p>오타쿠 특 : 혼자 벅차오름<br/>당신은 누구보다 순수한 마음을 가진 덕후</p>
+            </div>
+        </div>  
+        <div className='down'>
+            <div className="sns">
+                <p>몇 안되는 친구들에게 공유해보세요!</p>
+                <button className="kakao_btn" onClick={kakaoButton}/>
+                <FacebookShareButton style={{ marginLeft: "12px" }} url={"https://virtupiaotaku.netlify.app/"}>
+                <FacebookIcon size={48} round={true} borderRadius={24}></FacebookIcon>
+                </FacebookShareButton>
+                <LineShareButton style={{ marginLeft: "12px" }} url={"https://virtupiaotaku.netlify.app/"}>
+                    <LineIcon size={48} round={true} borderRadius={24}></LineIcon>
+                </LineShareButton>
+                <TwitterShareButton style={{ marginLeft: "12px" }} url={"https://virtupiaotaku.netlify.app/"}>
+                    <TwitterIcon size={48} round={true} borderRadius={24}></TwitterIcon>
+                </TwitterShareButton>
+                <button className="restart" onClick={restart}>인정할 수 없엉...!! 테스트 다시하기</button>
+            </div>
+            <div className='virtupia'>
+                <img src={virtupia}></img>
+                <div>
+                    <b>당신과 같은 사람들만 모여있어요!</b>
+                    <p>#함께하는 #버츄얼덕질 #버튜피아</p>
+                </div>
+            </div>
+            <div className='store'>
+                <button onClick={()=>window.open(appStoreUrl)}><img src={appstore}></img></button>
+                <button onClick={()=>window.open(googleStroeeUrl)}><img src={googlestore}></img></button>
+            </div>
+        </div>
+    </div>
+    );
+}
