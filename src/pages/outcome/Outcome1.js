99cf7eeaac20e776e26f2ba0f049b257f976468e
wonsunyoung <wonsunyoung0703@gmail.com>
2023-10-24 12:23:54 +0900

    Update Outcome1.js

--- src/pages/outcome/Outcome1.js
+++ src/pages/outcome/Outcome1.js
@@ -1,14 +1,10 @@
 import { useNavigate, useLocation, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import normal from '../../asset/image/normal.jpg'
-import virtupia from '../../asset/image/virtupia.png'
-import appstore from '../../asset/image/appstore.png'
-import googlestore from '../../asset/image/googleplay.png'
+import OutcomeDown from '../../component/OutcomeDown';
 import {FacebookShareButton, FacebookIcon, FacebookMessengerShareButton, FacebookMessengerIcon, TwitterShareButton, TwitterIcon, LineShareButton, LineIcon,} from "react-share";
 
 export default function Outcome1(){
-const appStoreUrl = "https://apps.apple.com/kr/app/%EB%B2%84%ED%8A%9C%ED%94%BC%EC%95%84/id6451460797"
-const googleStroeeUrl = "https://play.google.com/store/apps/details?id=com.virtuverseapp&pcampaignid=web_share"
 const navigate = useNavigate();
 const restart = () => {navigate('/')}
 const location = useLocation();
@@ -25,29 +21,27 @@ const kakaoButton = () => {
           title: '덕후 판독기',
           description: '나는 덕질에 얼마나 진심일까?',
           imageUrl:
-            'https://ifh.cc/g/rR3gVj.jpg',
+            'https://i.postimg.cc/C1JyjYg8/normal.jpg',
           link: {
-            webUrl: 'https://virtupiaotaku.netlify.app/',
+            webUrl: 'https://virtupiaotakutest.netlify.app/Outcome1',
           },
         },
-        social: {
-          likeCount: 286,
-          commentCount: 45,
-          sharedCount: 845,
-        },
+        // social: {
+        //   likeCount: 286,
+        //   commentCount: 45,
+        //   sharedCount: 845,
+        // },
         buttons: [
           {
             title: '웹으로 보기',
             link: {
-              webUrl: 'https://virtupiaotaku.netlify.app/',
+              webUrl: 'https://virtupiaotakutest.netlify.app/Outcome1',
             },
           },
         ],
       });
     }
   }
-
-
     return(
     <div className='result'>
         <div className='up'>
@@ -62,28 +56,18 @@ const kakaoButton = () => {
             <div className="sns">
                 <p>몇 안되는 친구들에게 공유해보세요!</p>
                 <button className="kakao_btn" onClick={kakaoButton}/>
-                <FacebookShareButton style={{ marginLeft: "12px" }} url={"https://virtupiaotaku.netlify.app/"}>
+                <FacebookShareButton style={{ marginLeft: "12px" }} url={"https://virtupiaotakutest.netlify.app/Outcome1"}>
                 <FacebookIcon size={48} round={true} borderRadius={24}></FacebookIcon>
                 </FacebookShareButton>
-                <LineShareButton style={{ marginLeft: "12px" }} url={"https://virtupiaotaku.netlify.app/"}>
+                <LineShareButton style={{ marginLeft: "12px" }} url={"https://virtupiaotakutest.netlify.app/Outcome1"}>
                     <LineIcon size={48} round={true} borderRadius={24}></LineIcon>
                 </LineShareButton>
-                <TwitterShareButton style={{ marginLeft: "12px" }} url={"https://virtupiaotaku.netlify.app/"}>
+                <TwitterShareButton style={{ marginLeft: "12px" }} url={"https://virtupiaotakutest.netlify.app/Outcome1"}>
                     <TwitterIcon size={48} round={true} borderRadius={24}></TwitterIcon>
                 </TwitterShareButton>
                 <button className="restart" onClick={restart}>인정할 수 없엉...!! 테스트 다시하기</button>
             </div>
-            <div className='virtupia'>
-                <img src={virtupia}></img>
-                <div>
-                    <b>당신과 같은 사람들만 모여있어요!</b>
-                    <p>#함께하는 #버츄얼덕질 #버튜피아</p>
-                </div>
-            </div>
-            <div className='store'>
-                <button onClick={()=>window.open(appStoreUrl)}><img src={appstore}></img></button>
-                <button onClick={()=>window.open(googleStroeeUrl)}><img src={googlestore}></img></button>
-            </div>
+            <OutcomeDown/>
         </div>
     </div>
     );
