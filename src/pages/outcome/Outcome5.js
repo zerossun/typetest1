4206e74f40b5778e3c2628801dada92b68b6cc16
wonsunyoung <wonsunyoung0703@gmail.com>
2023-10-24 18:12:25 +0900

    Update Outcome5.js

--- src/pages/outcome/Outcome5.js
+++ src/pages/outcome/Outcome5.js
@@ -4,6 +4,7 @@ import fourth from '../../asset/image/fourth.jpeg'
 import Facebook from '../../asset/image/facebook.png';
 import line from '../../asset/image/line.png'
 import twitter from '../../asset/image/twitter.png'
+import share from '../../asset/image/share.png'
 import OutcomeDown from '../../component/OutcomeDown';
 import {FacebookShareButton, FacebookIcon, FacebookMessengerShareButton, FacebookMessengerIcon, TwitterShareButton, TwitterIcon, LineShareButton, LineIcon,} from "react-share";
 
@@ -39,6 +40,19 @@ const kakaoButton = () => {
       });
     }
   }
+  const handle = () => {
+    if (navigator.share) {
+        navigator.share({
+            title: '덕후 판독기',
+            text: '나는 덕질에 얼마나 진심일까?',
+            url: 'https://virtupiaotakutest.netlify.app/Outcome5',
+        });
+    }else{
+        alert("공유하기가 지원되지 않는 환경 입니다.")
+    }
+  }
+  
+
    return(
     <div className='result'>
         <div className='up'>
@@ -52,14 +66,15 @@ const kakaoButton = () => {
         <div className='down'>
             <div className="sns">
                 <p>몇 안되는 친구들에게 공유해보세요!</p>
-                <button className="kakao_btn" onClick={kakaoButton}/>
-                <FacebookShareButton style={{ width:'50px', marginLeft: "12px" }} url={"https://virtupiaotakutest.netlify.app/Outcome5"}>
+                <button className='share' onClick={()=>{handle()}}><img src={share} style={{width:'50px'}}></img></button>
+                <button className="kakao_btn" style={{ marginLeft: "16px" }} onClick={kakaoButton}/>
+                <FacebookShareButton style={{ width:'50px', marginLeft: "16px" }} url={"https://virtupiaotakutest.netlify.app/Outcome5"}>
                 <img src={Facebook} style={{width:'50px'}}></img>
                 </FacebookShareButton>
-                <LineShareButton style={{ marginLeft: "12px" }} url={"https://virtupiaotakutest.netlify.app/Outcome5"}>
+                <LineShareButton style={{ marginLeft: "16px" }} url={"https://virtupiaotakutest.netlify.app/Outcome5"}>
                     <img src={line} style={{width:'50px'}}></img>
                 </LineShareButton>
-                <TwitterShareButton style={{ marginLeft: "12px" }} url={"https://virtupiaotakutest.netlify.app/Outcome5"}>
+                <TwitterShareButton style={{ marginLeft: "16px" }} url={"https://virtupiaotakutest.netlify.app/Outcome5"}>
                     <img src={twitter} style={{width:'50px'}}></img>
                 </TwitterShareButton>
                 <button className="restart" onClick={restart}>인정할 수 없엉...!! 테스트 다시하기</button>
