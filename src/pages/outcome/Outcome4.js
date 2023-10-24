0637cd542341318eb8f1ac1c9248347b151928d5
wonsunyoung <wonsunyoung0703@gmail.com>
2023-10-24 16:40:28 +0900

    Update Outcome4.js

--- src/pages/outcome/Outcome4.js
+++ src/pages/outcome/Outcome4.js
@@ -1,6 +1,9 @@
 import { useNavigate, useLocation, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import third from '../../asset/image/third.jpg'
+import Facebook from '../../asset/image/facebook.png';
+import line from '../../asset/image/line.png'
+import twitter from '../../asset/image/twitter.png'
 import OutcomeDown from '../../component/OutcomeDown';
 import {FacebookShareButton, FacebookIcon, FacebookMessengerShareButton, FacebookMessengerIcon, TwitterShareButton, TwitterIcon, LineShareButton, LineIcon,} from "react-share";
 
@@ -14,7 +17,6 @@ const kakaoButton = () => {
       if (!kakao.isInitialized()) {
         kakao.init('206dac13674d5431132f3d0db85f378f')
       }
-
       kakao.Share.sendDefault({
         objectType: 'feed',
         content: {
@@ -37,8 +39,6 @@ const kakaoButton = () => {
       });
     }
   }
-
-
     return(
     <div className='result'>
         <div className='up'>
@@ -54,18 +54,18 @@ const kakaoButton = () => {
                 <p>몇 안되는 친구들에게 공유해보세요!</p>
                 <button className="kakao_btn" onClick={kakaoButton}/>
                 <FacebookShareButton style={{ marginLeft: "12px" }} url={"https://virtupiaotakutest.netlify.app/Outcome4"}>
-                <FacebookIcon size={48} round={true} borderRadius={24}></FacebookIcon>
+                  <img src={Facebook} style={{width:'50px'}}></img>
                 </FacebookShareButton>
                 <LineShareButton style={{ marginLeft: "12px" }} url={"https://virtupiaotakutest.netlify.app/Outcome4"}>
-                    <LineIcon size={48} round={true} borderRadius={24}></LineIcon>
+                  <img src={line} style={{width:'50px'}}></img>
                 </LineShareButton>
                 <TwitterShareButton style={{ marginLeft: "12px" }} url={"https://virtupiaotakutest.netlify.app/Outcome4"}>
-                    <TwitterIcon size={48} round={true} borderRadius={24}></TwitterIcon>
+                  <img src={twitter} style={{width:'50px'}}></img>
                 </TwitterShareButton>
                 <button className="restart" onClick={restart}>인정할 수 없엉...!! 테스트 다시하기</button>
             </div>
-            <OutcomeDown/>
         </div>
+        <OutcomeDown/>
     </div>
     );
 }
